3ef850543e84fb8d610cb0cf10bc5be1dd9456ca
Rajesh Radhakrishnan <rajeshmvk@gmail.com>
2022-06-08 19:29:58 +0530

    fix for CORS

--- kitchen-web/texty/server.sh
+++ kitchen-web/texty/server.sh
@@ -6,4 +6,4 @@ then
 fi
 
 #rasa run --enable-api --port $PORT
-rasa run -p $PORT -m models --credentials credentials.yml --enable-api --log-file out.log --endpoints endpoints.heroku.yml
+rasa run -p $PORT -m models --credentials credentials.yml --enable-api --log-file out.log --endpoints endpoints.heroku.yml --cors "*"
