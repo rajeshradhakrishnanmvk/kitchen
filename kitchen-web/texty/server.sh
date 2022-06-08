bc1f0daf62ebaec17c543b2fb6783398734dd239
Rajesh Radhakrishnan <rajeshmvk@gmail.com>
2022-06-08 20:59:21 +0530

    corrected configs for rasa & firebase

--- kitchen-web/texty/server.sh
+++ kitchen-web/texty/server.sh
@@ -6,4 +6,4 @@
 # fi
 
 #rasa run --enable-api --port $PORT
-rasa run -p $PORT -m models --credentials credentials.yml --log-file out.log --cors "*" --enable-api --endpoints endpoints.heroku.yml
+rasa run -p $PORT -m models --credentials credentials.yml --log-file out.log --cors "*" --enable-api --debug --endpoints endpoints.heroku.yml
