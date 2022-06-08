3dc44fbead98934d20c3d262b6672435aa4b6f1f
Rajesh Radhakrishnan <rajeshmvk@gmail.com>
2022-06-08 20:37:49 +0530

    removed the conditional statement

--- kitchen-web/texty/server.sh
+++ kitchen-web/texty/server.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-if [ -z "$PORT"]
-then
-  PORT=5005
-fi
+# if [ -z "$PORT"]
+# then
+#   PORT=5005
+# fi
 
 #rasa run --enable-api --port $PORT
 rasa run -p $PORT -m models --credentials credentials.yml --log-file out.log --cors "*" --enable-api --endpoints endpoints.heroku.yml
